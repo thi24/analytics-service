d13349d62d58caff3f05ab47d8bbd73c39c7c962
Andreas Dinauer <andreas.j.dinauer@gmail.com>
2024-05-12 19:20:17 +0200

    patch request added to increment view count. endpoints not secured yet

--- src/main/java/de/benevolo/repo/EventViewRepo.java
+++ src/main/java/de/benevolo/repo/EventViewRepo.java
@@ -5,13 +5,19 @@ import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 public class EventViewRepo implements PanacheRepositoryBase<EventView, String> {
 
     public List<EventView> findByEventId(String eventId) {
         return list("eventId = :eventId", Parameters.with("eventId", eventId));
+    }
+
+    public EventView findByEventIdAndOccurringDate(String eventId, LocalDate occurringDate) {
+        return list("eventId = :eventId and occurringDate = :occurringDate", Parameters.with("eventId", eventId).and("occurringDate", occurringDate)).stream().findFirst().orElse(new EventView(eventId, occurringDate));
     }
 
 }
