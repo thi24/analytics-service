952b66a26a8fe5944cccbc8889d458ef43e3a40d
Andreas Ott <andreas.konrad.ott@googlemail.com>
2024-05-14 12:45:12 +0200

    added create table sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,10 @@
+CREATE TABLE event_view (
+    id UUID PRIMARY KEY,
+    event_id UUID NOT NULL,
+    occurring_date DATE NOT NULL,
+    views INT NOT NULL
+);
+
 INSERT INTO event_view(id, event_id, occurring_date, views)
 SELECT gen_random_uuid(), '383f700f-5449-4e40-b509-bee0b5d139d6' , (NOW() - (floor(random() * 120) || ' days')::interval)::date
      , floor(random() * 2900 + 100)::int
